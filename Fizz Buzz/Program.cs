4d7d8b42944feba1ef098d2c2cbace0d2665bb21
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2022-12-06 14:03:16 +0100

    Add project files.

---
+++ Fizz Buzz/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Welcome to the Fizz Buzz Program");
+
+Console.Write("Enter the value: ");
+int n = int.Parse(Console.ReadLine());
+FizzBuzz(n);
+void FizzBuzz (int n) {
+   
+
+    for(int i = 1; i <= n; i++) {
+        if ((i % 3 == 0) && (i % 5 == 0))
+        {
+            Console.WriteLine("Fizz Buzz");
+        }
+        else if (i % 3 == 0)
+        {
+            Console.WriteLine("Fizz");
+        }
+        else if (i % 5 == 0)
+        {
+            Console.WriteLine("Buzz");
+        }
+        else {
+            Console.WriteLine($"{i}");
+        }
+        
+    }
+}
+
+
